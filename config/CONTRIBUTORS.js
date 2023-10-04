c76b55805832b940c852e4b0e23f9075896e4fa4
Arindal Char <110285827+arindal1@users.noreply.github.com>
2023-10-04 14:51:08 +0530

    Create CONTRIBUTORS.js

---
+++ config/CONTRIBUTORS.js
@@ -0,0 +1,17 @@
+# Keep the formating same
+
+const contributors = [
+  {
+    name: 'Arindal',
+    avatarUrl: 'https://github.com/arindal1.png',
+    githubLink: 'https://github.com/arindal1'
+  },
+  {
+    name: 'Simon Sandors',
+    avatarUrl: 'https://github.com/simanders.png',
+    githubLink: 'https://github.com/simanders'
+  }
+  // Add more contributors as needed
+];
+
+export default contributors;
